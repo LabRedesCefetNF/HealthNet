274f2d97c4a570396b86beb35c48f67a0bf18a3d
Irlon <irloncefet@gmail.com>
2019-10-24 21:33:37 -0300

    ALtera função de armazemaneto para obter chaves

--- armazenar.php
+++ armazenar.php
@@ -3,6 +3,7 @@ $uploadDir = 'uploads/';
 $idDono = $_POST['idDono'];
 $idRelacionado = $_POST['idRelacionado'];
 $senha = $_POST['senha'];
+$chavePrivadaCifrada = $_FILES['chave'];
 
 $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 $pdo = new PDO('mysql:host=127.0.0.1;dbname=healthnet','root','',$options);
@@ -18,6 +19,23 @@ move_uploaded_file($_FILES["arquivo"]["tmp_name"], $targetFilePath);
 //gera chave Diffie-Hellman
 //transforma em base64
 //cifra utilizando chave Diffie-Hellman
+
+$sql = 'select cPublica from usuario where id=?';
+$ps = $pdo->prepare($sql);
+$ps->execute(array($idRelacionado));
+
+$chavesPublicas = array();
+			
+while($obj = $ps->fetchObject()){				
+    array_push($chavesPublicas, $obj);
+}
+
+$cPublica = $chavesPublicas[0]->cPublica;
+
+file_put_contents ('temp-keys/publicKey.pem', $cPublica);
+move_uploaded_file($_FILES["chave"]["tmp_name"], 'temp-keys/privateKeyEncrypted.pem');
+
+system('cd ./shell/ && ./encrypt.sh '.$senha.' '.$fileName);
 
 //salva relação
 $sql = 'insert into relacao(idDono, idRelacionado) values(?,?)';
--- shell/encrypt.sh
+++ shell/encrypt.sh
@@ -1,1 +1,3 @@
 #! /bin/bash
+
+openssl enc -aes-256-cbc -d -pass pass:$1 -p -in ../temp-keys/privateKeyEncrypted.pem -out ../temp-keys/privateKey.pem
