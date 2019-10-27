fa6838a413ba06e88d45209b08725ef3516d7377
Irlon <irloncefet@gmail.com>
2019-10-27 12:11:46 -0300

    Altera função de armazenar

--- shell/encrypt.sh
+++ shell/encrypt.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
+openssl aes-256-cbc -d -k $1 -in ../temp-keys/privateKeyEncrypted.pem -out ../temp-keys/privateKey.pem
 
-openssl enc -aes-256-cbc -d -pass pass:$1 -p -in ../temp-keys/privateKeyEncrypted.pem -out ../temp-keys/privateKey.pem
+openssl pkeyutl -derive -inkey ../temp-keys/privateKey.pem -peerkey ../temp-keys/publicKey.pem -out ../temp-keys/chaveDH.bin
+
+base64 ../uploads/$2 > ../uploads/encoded.txt
+
+openssl aes-256-cbc -e -kfile ../temp-keys/chaveDH.bin -in ../uploads/encoded.txt -out ../saved-files/$2
+
+#base64 -d ../uploads/encoded.txt > ../uploads/decoded.txt
--- shell/generatekeys.sh
+++ shell/generatekeys.sh
@@ -9,4 +9,4 @@ fi
 openssl genpkey -paramfile chaveglobal.pem -out ../temp-keys/privateKey.pem
 openssl pkey -in ../temp-keys/privateKey.pem -pubout -out ../temp-keys/publicKey.pem
 
-openssl enc -aes-256-cbc -pass pass:$1 -p -in ../temp-keys/privateKey.pem -out ../temp-keys/privateKeyEncrypted.pem
+openssl aes-256-cbc -e -k $1 -in ../temp-keys/privateKey.pem -out ../temp-keys/privateKeyEncrypted.pem
